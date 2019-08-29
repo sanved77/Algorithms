afd307cf6d61d5e3dc7d25ae51df17b2b58dfb01
sanved77 <sanved77@gmail.com>
2019-08-29 16:23:42 -0400

    Added iteration data

--- CutRod.java
+++ CutRod.java
@@ -8,18 +8,22 @@ public class CutRod {
 
         result[0] = 0;
         
-        for(int i = 1; i <= 5;i++){
+        for(int i = 1; i <= prices.length;i++){
             int max = -1;
             for(int j = 1 ; j <= i; j++){
                 int temp = prices[j-1] + result[i - j];
+                System.out.println("[i = " + i + ", j = " + j + "] : p[" + j + " - 1]" + " R[" + i + " - " + j + " ] = " + " p[" + (j-1) + "] + R[" + (i-j) + "] = "  + (prices[j-1] + result[i - j]) 
+                + "; max = " + temp);
                 if(temp > max) max = temp;
             }
             result[i] = max;
         }
 
+        System.out.print("\nResults - [");
         for(int a : result){
-            System.out.println(" - " + a);
+            System.out.print(a + ",");
         }
+        System.out.print("]");
 
     }
 
