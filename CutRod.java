f0f61ad44a06bb9e04a2417a6b35bcb2ff0b5910
sanved77 <sanved77@gmail.com>
2019-08-29 22:19:57 -0400

    Added rod piece sizes

--- CutRod.java
+++ CutRod.java
@@ -2,21 +2,35 @@ public class CutRod {
 
     public static void main(String ag[]){
 
-        int[] prices = {1,5,8,9,10};
+        // int[] prices = {50, 2, 40, 3, 4, 1, 6, 7};
+        // int[] prices = {2,3,7,8,10};
+        // int[] prices = {1,5,8,9,10};
+        int[] prices = {1,5,8,9,10, 17, 17, 20};
 
         int[] result = new int[prices.length + 1];
+
+        int[] table = new int[prices.length + 1];
 
         result[0] = 0;
         
         for(int i = 1; i <= prices.length;i++){
-            int max = -1;
+            int max = -1; int sTemp;
             for(int j = 1 ; j <= i; j++){
                 int temp = prices[j-1] + result[i - j];
-                System.out.println("[i = " + i + ", j = " + j + "] : p[" + j + " - 1]" + " R[" + i + " - " + j + " ] = " + " p[" + (j-1) + "] + R[" + (i-j) + "] = "  + (prices[j-1] + result[i - j]) 
-                + "; max = " + temp);
-                if(temp > max) max = temp;
+                System.out.println("[i = " + i + ", j = " + j + "] : p[" + j + " - 1]" + " R[" + i + " - " + j + "] = " + " p[" + (j-1) + "] + R[" + (i-j) + "] = "  + (prices[j-1] + result[i - j]) 
+                + "; priceNow = " + temp);
+                if(temp > max) {
+                    max = temp;
+                    table[i] = j;
+                }
             }
+            System.out.println("");
             result[i] = max;
+        }
+        int n = prices.length;
+        while(n != 0){
+            System.out.println(" - " + table[n]);
+            n -= table[n];
         }
 
         System.out.print("\nResults - [");
