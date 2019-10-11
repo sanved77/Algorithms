a04324faab95952c9a207bf2856d57f72caf75a1
sanved77 <sanved77@gmail.com>
2019-10-11 18:26:27 -0400

    LinkedList implemented

--- LinkedList.java
+++ LinkedList.java
@@ -10,13 +10,19 @@ public class LinkedList {
         for(int a : arr){
             insertNode(a);
         }
-
+        
         printNodes();
+        deleteNode(5);
+        printNodes();
+
+        
     }
 
     public static void insertNode(int num){
         Node head = root;
-        if(head == null) return;
+        if(head == null) {
+            root = new Node(num);
+        }
         while(head.next != null){
             head = head.next;
         }
@@ -24,15 +30,47 @@ public class LinkedList {
         Node temp = new Node(num);
         temp.next = null;
         head.next = temp;
+    }
 
+    public static void deleteNode(int num){
+        Node head = root;
+        if(head == null) return;
+        if(head.num == num){
+            root = head.next;
+        }
+        // if(head.next == null){
+        //     if(head.num == num){
+        //         root = null;
+        //         System.out.println("Element at root deleted");
+        //     }else{
+        //         System.out.println("Nothing to delete");
+        //         return;
+        //     }
+        // }
+        Node prev = head;
+        head = head.next;
+        while(head.next != null){
+            if(head.num == num){
+                prev.next = head.next;
+                break;
+            }else{
+                prev = head;
+                head = head.next;
+            }
+        }
+        if(head.num == num){
+            prev.next = head.next;
+        }
     }
 
     public static void printNodes(){
         Node ptr = root;
+        System.out.println("");
         while(ptr.next != null)  {
-            System.out.println(" - " + ptr.num);
+            System.out.print(ptr.num + ",");
             ptr = ptr.next;
-        }System.out.println(" - " + ptr.num);
+        }
+        System.out.print(ptr.num + ",");
     }
 
     static class Node{
