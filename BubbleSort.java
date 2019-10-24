d6521eb79945ce886f5ef8aaefa380319e3286e2
sanved77 <sanved77@gmail.com>
2019-10-23 21:36:06 -0400

    Bubble sort optimzed

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,15 +3,21 @@ class BubbleSort{
     static int arr[] = {14, 5, 87, 12, 7, 2, 26, 9, 3};
 
     public static void main(String[] args) {
+
+        int count = 0;
         
         for(int i = 0 ; i < arr.length; i++){
+            boolean swapped = false;
             for(int j = 0 ; j < arr.length-1; j++){
+                count++;
                 if(arr[j] > arr[j+1]){
                     swap(j,j+1);
+                    swapped = true;
                 }
             }
+            if(swapped) break;
         }
-
+        System.out.println("" + count + "!\n");
         printArr();
 
     }
